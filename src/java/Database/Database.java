1467d82908c873ecc6641c9452f8cf95461f0d00
WesleyI <wesleyingenbleek@live.nl>
2017-09-21 11:15:06 +0200

    Fixed DB connection

--- src/java/Database/Database.java
+++ src/java/Database/Database.java
@@ -1,19 +1,13 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Database;
 
-import Feedback.Feedback;
-import Login.User;
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +17,13 @@ import java.util.logging.Logger;
  * @author wesle
  */
 public class Database {
+    
+     private static final String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11195566";
+     private static final String username = "sql11195566";
+     private static final String password = "EVS7xj7jdW";
+     
 
+    
     /**
      * Connects with the database.
      *
@@ -32,11 +32,10 @@ public class Database {
      */
     public static Connection getConnection() throws SQLException {
         try {
-            String url = "jdbc:mysql://sql11.freemysqlhosting.net:3306/sql11194356";
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            Connection con = DriverManager.getConnection(url, "sql11194356", "YpVPS7iJwE");
+            Connection con = DriverManager.getConnection(url, username, password);
             return con;
-        } catch (Exception ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         }
