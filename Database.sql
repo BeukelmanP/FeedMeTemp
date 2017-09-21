d9e74bf994ee2e38f06a2fe662cdb64ff46c7cf7
WesleyI <wesleyingenbleek@live.nl>
2017-09-21 11:30:56 +0200

    Updated SQL script

--- Database.sql
+++ Database.sql
@@ -12,14 +12,18 @@ CREATE TABLE `user`
 `email` VARCHAR(255) NOT NULL,
 `password` VARCHAR(255) NOT NULL,
 `score` INT(4) DEFAULT 0,
+`department` VARCHAR(255),
+`image` VARCHAR(255),
 PRIMARY KEY(id)
 );
 
 CREATE TABLE `feedback`
 (
 `id` INT(4) auto_increment,
-`sendTo` INT(4),
-`sendFrom` INT(4),
+`sendTo` INT(4) NOT NULL,
+`sendFrom` INT(4) NOT NULL,
+`tips` VARCHAR(255),
+`tops` VARCHAR(255),
 `feedback` VARCHAR(255) NOT NULL,
 `timeCreated` DATETIME NOT NULL,
 PRIMARY KEY(id),
@@ -38,17 +42,17 @@ FOREIGN KEY (feedbackId) REFERENCES `feedback`(id),
 FOREIGN KEY (likeFrom) REFERENCES `user`(id)
 );
 
-INSERT INTO `user` (id,firstname,lastname,email,password)
-VALUES (1,'test', 'test', 'test', 'test');
-INSERT INTO `user` (id,firstname,lastname,email,password)
-VALUES (2, 'wes', 'wes', 'wes@wes.nl', 'wes'); 
+INSERT INTO `user` (id,firstname,lastname,email,password,department,image)
+VALUES (1,'test', 'test', 'test', 'test','test','test');
+INSERT INTO `user` (id,firstname,lastname,email,password,department,image)
+VALUES (2, 'wes', 'wes', 'wes@wes.nl', 'wes','dep','image'); 
 
-INSERT INTO `feedback` (id,sendTo,sendFrom,feedback,timeCreated)
-VALUES (1,1,2,'lekker gewerkt pik',CURRENT_TIMESTAMP());
-INSERT INTO `feedback` (id,sendTo,sendFrom,feedback,timeCreated)
-VALUES (2,2,1, 'Feedback all the way',CURRENT_TIMESTAMP());
-INSERT INTO `feedback` (id,sendTo,sendFrom,feedback,timeCreated)
-VALUES (3,2,1, 'Je stinkt',CURRENT_TIMESTAMP());
+INSERT INTO `feedback` (id,sendTo,sendFrom,tips,tops,feedback,timeCreated)
+VALUES (1,1,2,'tip','top','lekker gewerkt pik',CURRENT_TIMESTAMP());
+INSERT INTO `feedback` (id,sendTo,sendFrom,tips,tops,feedback,timeCreated)
+VALUES (2,2,1,'tip','top', 'Feedback all the way',CURRENT_TIMESTAMP());
+INSERT INTO `feedback` (id,sendTo,sendFrom,tips,tops,feedback,timeCreated)
+VALUES (3,2,1,'tip','top', 'Je stinkt',CURRENT_TIMESTAMP());
 
 INSERT INTO `user_feedback` (id,feedbackId,likeFrom,helpful)
 VALUES (1,1,1,1);
@@ -57,9 +61,11 @@ VALUES (2,1,2,1);
 
 
 
-/*Server: sql11.freemysqlhosting.net
-Name: sql11194356
-Username: sql11194356
-Password: YpVPS7iJwE
+
+/*
+Server: sql11.freemysqlhosting.net
+Name: sql11195566
+Username: sql11195566
+Password: EVS7xj7jdW
 Port number: 3306
 */
