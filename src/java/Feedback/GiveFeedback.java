e802147497cb3ba8898e772de80d20b8d12f6efe
Berghs,Sander S.C.R.H. <s.berghs@student.fontys.nl>
2017-10-03 14:08:07 +0200

    FrontEnd (Un)Helpful

--- src/java/Feedback/GiveFeedback.java
+++ src/java/Feedback/GiveFeedback.java
@@ -6,7 +6,7 @@
 package Feedback;
 
 import static Database.FeedbackDatabase.InsertFeedback;
-import javax.enterprise.context.SessionScoped;
+import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
